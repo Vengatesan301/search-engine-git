4a084c76c447bec57566f649b7209fc910c6adad
Vengatesan301 <kit.24.20bcs057@gmail.com>
2023-04-04 09:47:21 +0530

    five

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,10 @@ import React from 'react'
     return(
     <div className='ui raised very padded text container segment' style={{marginTop:'80px'}}> 
         <h1>contact us</h1>
+        <p>Have questions? Shoot us an email.
+We are an industry-leading publication that provides the latest news and insights about SEO, search, and how it impacts your business -and career.
+
+Have a question for us or feedback? Please click on the most appropriate category and fill out the form to reach us.</p>
     </div>
     )
  }
